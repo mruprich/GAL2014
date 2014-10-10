78dbed1c5f21b39e353591e9d79c4f1ae3c5e3a6
Daniel Javorsky <javorskydanniel@gmail.com>
2014-10-10 12:53:10 +0200

    Attemp #1.1

--- src/Fleury/main.cpp
+++ src/Fleury/main.cpp
@@ -1,6 +1,9 @@
 #include "mainwindow.h"
 #include <QApplication>
+/*
+Int main, tak co bycshom se tu asi tak dozvedeli... mozna to, ze jsme zprovoznili GIT v oknech?
 
+*/
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
